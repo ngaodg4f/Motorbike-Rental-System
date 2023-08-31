ca4e79e7c58b43f8a759a43b4bc7a0b64269e1d4
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2023-08-31 20:35:14 +0700

    refactor: adding std::string

--- User/User.h
+++ User/User.h
@@ -5,28 +5,28 @@
 
 class User {
     private:
-        string fullname {};
-        string username {};
-        string password;
-        string phone;
-        string idType;
-        string id;
-        string driver_license;
+        std::string fullname {};
+        std::string username {};
+        std::string password;
+        std::string phone;
+        std::string idType;
+        std::string id;
+        std::string driver_license;
         // Date expiry_date; // wait for Date class
         // vehicle_list // wait for Motorbike class
         // int credit_point; // wait for uml
 
     public:
         User() = default;
-        User(string username, string password);
+        User(std::string username, std::string password);
         ~User();
-        void set_fullname(string fullname);
-        void set_username(string username);
-        void set_password(string password);
-        void set_phone(string phone);
-        void set_idType(string id_type); 
-        void set_id(string id);
-        void set_driver_license(string driver_license);
+        void set_fullname(std::string fullname);
+        void set_username(std::string username);
+        void set_password(std::string password);
+        void set_phone(std::string phone);
+        void set_idType(std::string id_type); 
+        void set_id(std::string id);
+        void set_driver_license(std::string driver_license);
         // void set_expiry_date(Date expiry_date); // wait for Date class
         
         virtual void list_vehicle() = 0;
