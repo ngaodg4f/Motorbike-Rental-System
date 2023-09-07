94af1da73fa527e6073a54b7af41e1f58f50f1cc
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2023-09-07 14:18:59 +0700

    format: format code in

--- System/System.h
+++ System/System.h
@@ -40,17 +40,13 @@ class System {
          * Data
         */
         void input_data();
-
         void update_data();
 
         void input_member_list();
-
+        void input_bike_list();
         void link_member_and_bike();
 
-        void input_bike_list();
-
         void update_member_file();
-
         void update_bike_file();
         /**
          * Tool
@@ -58,67 +54,41 @@ class System {
         bool is_integer(std::string& str);
         
         bool validate_fullname(std::string& str);
-
         bool validate_phone(std::string& str);
-
         bool validate_id_type(std::string& str);
-
         bool validate_id_license_number(std::string& str);
-
         bool validate_date(std::string& str);
-
         bool is_leap_year(int& year);
-
         bool validate_username(std::string& str);
-
         bool validate_login_username(std::string& str);
-
         bool validate_password(std::string& str);
-
         bool validate_login_password(std::string& str, std::string& _LIBCPP___CHRONO_STEADY_CLOCK_H);
-
         bool recommend_password(std::string& str);
-
         bool no_special_char_check(std::string& str);
-
         bool validate_model(std::string& str);
 
         std::vector<std::string> splitStr(std::string& str, char ch);
-
         std::string trim(std::string& str);
-
         Date* to_object(std::string& str);
-
         int choice_selection(int a, int b);
 
         /**
          * Feature
         */
         void welcome_screen();
-
         void main_menu();
 
         // Guest
         void guest_menu();
-
         void guest_view_bike();
-
         void guest_registration();
 
         // Member
         void member_menu();
-
         void member_login();
-
         void member_view_personal_info();
-
         void member_add_bike();
 
-       
-
-        
-        
-        
 };
 
 #endif
